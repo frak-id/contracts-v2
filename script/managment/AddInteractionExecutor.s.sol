cd935ca56c1d5066b417cabfbaefb1908d437105
KONFeature <quentin@nivelais.com>
2024-09-29 17:54:53 +0200

    🚀 Deploy to testnet

--- script/managment/AddInteractionExecutor.s.sol
+++ script/managment/AddInteractionExecutor.s.sol
@@ -7,7 +7,8 @@ import "forge-std/console.sol";
 import {DELEGATION_EXECUTOR_ROLE, InteractionDelegator} from "src/kernel/interaction/InteractionDelegator.sol";
 
 contract AddInteractionExecutor is Script, DeterminedAddress {
-    address private operator = 0x0612994c389F253f22AF91B63DD622049b7D42C5;
+    address private operatorLocal = 0x0612994c389F253f22AF91B63DD622049b7D42C5;
+    address private operatorDev = 0xef33C59086808F63733C3b92d273930772466b08;
 
     function run() public {
         KernelAddresses memory addresses = _getKernelAddresses();
@@ -19,7 +20,8 @@ contract AddInteractionExecutor is Script, DeterminedAddress {
 
     function _addExecutor(InteractionDelegator _interactionDelegator) internal {
         vm.startBroadcast();
-        _interactionDelegator.grantRoles(operator, DELEGATION_EXECUTOR_ROLE);
+        _interactionDelegator.grantRoles(operatorLocal, DELEGATION_EXECUTOR_ROLE);
+        _interactionDelegator.grantRoles(operatorDev, DELEGATION_EXECUTOR_ROLE);
         vm.stopBroadcast();
     }
 }
