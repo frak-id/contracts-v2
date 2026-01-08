9aaf6ce2b39cdf45055b5e86908bcd4569a1a7c1
KONFeature <quentin@nivelais.com>
2026-01-08 18:29:04 +0100

    🔥 Remove v1 contracts

--- src/campaign/libs/RewardChainingCampaign.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GNU GPLv3
-pragma solidity 0.8.23;
-
-/// @dev Representing the reward chaining config
-struct RewardChainingConfig {
-    uint256 userPercent;
-    uint256 deperditionPerLevel;
-}
